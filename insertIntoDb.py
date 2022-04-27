d4f2ac7ea71a1cbe0ca31ff105e5597ff98d8366
rranjan6 <rahul.ranjan6344@gmail.com>
2022-04-27 20:45:51 +0530

    updated setting

--- insertIntoDb.py
+++ insertIntoDb.py
@@ -1,15 +1,25 @@
 # Import required modules
 import csv
 import sqlite3
+import psycopg2
+
+connection = psycopg2.connect(database="d8cpvu6blcd0la",
+						user='aziifjamteljua',
+						password='c0bf00eb34cc5f361cd4453321bd073aeac6e1aee8a02d6b954f34cac0122c88',
+						host='ec2-52-3-200-138.compute-1.amazonaws.com',
+						port='5432')
+
+connection.autocommit = True
+cursor = connection.cursor()
 
 
 # This File is used to  insert data into the data base using the csv files
 # Connecting to the geeks database
-connection = sqlite3.connect('db.sqlite3')
+# connection = sqlite3.connect('db.sqlite3')
 
 # Creating a cursor object to execute
 # SQL queries on a database table
-cursor = connection.cursor()
+# cursor = connection.cursor()
 
 # Table Definition
 
